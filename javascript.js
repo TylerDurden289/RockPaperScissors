cf34b4e9c17c15f6a647f1b348449fa3247c75e7
TylerDurden289 <258813223+TylerDurden289@users.noreply.github.com>
2026-03-20 18:44:57 +0100

    add scorecard and player buttons
    3 player buttons added for choices, reset game button added when any score reaches 5, scorecard added.

--- javascript.js
+++ javascript.js
@@ -2,25 +2,40 @@
     function getComputerChoice(){
         const RandomNum = Math.random()
         if (RandomNum < 0.33){
-            return("Rock")}
+            return "Rock";}
         else if (RandomNum < 0.66){
-            return("Scissors")}
-        else {return("Paper")};
+            return "Scissors";}
+        else {return "Paper";};
     }
 
 // this function asks the human to type his choice and returns the result
+let humanChoice=""
     function getHumanChoice(){
-        let userInput = prompt("Type Rock, Paper or Scissors");
-        return (userInput);
+        const Rock= document.querySelector("#Rock");
+        const Paper = document.querySelector("#Paper");
+        const Scissors = document.querySelector("#Scissors");
+    
+       Rock.addEventListener("click", () => {
+        humanChoice = "Rock"
+        playRound(humanChoice, getComputerChoice)
+    })
+       Paper.addEventListener("click", ()=> {
+        humanChoice = "Paper"
+        playRound(humanChoice,getComputerChoice)
+    })
+       Scissors.addEventListener("click", ()=> {
+        humanChoice = "Scissors"
+        playRound(humanChoice,getComputerChoice)
+    })
     };
 
     let humanScore = 0;
     let computerScore = 0;
 
 // this function is 1 round of the game, it takes the computer and human choice, trims them and puts them to uppercase so they are always comparable. Then it defines who wins at what condition via if else statements. It also prints the human and computer choice in the console(not required, just a check)
-    function playRound(humanChoice,computerChoice){
+    function playRound(humanChoice,getComputerChoice){
         const human = humanChoice.trim().toUpperCase();
-        const computer = computerChoice.trim().toUpperCase();
+        const computer = getComputerChoice().trim().toUpperCase();
 
         if ((human === "ROCK" && computer === "SCISSORS") || (human === "SCISSORS" && computer === "PAPER") || (human === "PAPER" && computer === "ROCK") ){
             console.log("Congrats, you win this round!");
@@ -38,30 +53,71 @@
 
         console.log("Human:",human);
         console.log("Computer:",computer);
+        
+        scoreCard(humanScore,computerScore)
     }
 
-// This function is the actual game it calls the computer and human to make a choice 5 times and plays 1 round with every given choice. It then compares scores and says who wins.
+// make a scorecard and show message when either score reaches 5
+function scoreCard(humanScore,computerScore){
+    const results= document.querySelector("#results")
 
-    function playGame(){
-    for (let round = 0; round < 5; round++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection,computerSelection);
-    }
-
-    console.log("Human final Score:", humanScore)
-    console.log("Computer final score:", computerScore)
-
-    if (humanScore > computerScore){
-        console.log("Game end, congratulations, you won!")
-    }
-    else if(humanScore < computerScore){
-        console.log("Game end, you lost, better luck next game")}
+    const existingScoreElements = document.querySelectorAll("#humanscore, #computerscore, #congrats, #loser, #draw");
+    existingScoreElements.forEach((element) => {
+        element.remove();
+    });
     
-    else{
-        console.log("It's a draw")
+    const humanScoreElement = document.createElement("p")
+    humanScoreElement.id = "humanscore"
+    humanScoreElement.textContent = `Human Score: ${humanScore}`
+    results.appendChild(humanScoreElement);
+
+    const computerScoreElement= document.createElement("p")
+    computerScoreElement.id = "computerscore";
+    computerScoreElement.textContent = `Computer Score: ${computerScore}`
+    results.appendChild(computerScoreElement)
+
+
+    if (humanScore>= 5){
+        const congrats = document.createElement("p")
+        congrats.id = "congrats"
+        congrats.textContent = 'Congratulations, you win this game!'
+        results.appendChild(congrats);
+        resetGame();
     }
+    else if (computerScore >= 5){
+         const loser = document.createElement("p");
+         loser.id = "loser"
+         loser.textContent = "Wow you kinda suck, you lost this game.";
+         results.appendChild(loser)
+         resetGame();
+    }
+    else if (computerScore === 5 && humanScore === 5){
+        const draw= document.createElement("p")
+        draw.id = "draw"
+        draw.textContent = "Its a draw, not great, not bad either!";
+        results.appendChild(draw)
+        resetGame();
     }
 
-playGame()
+
+}
+// make Reset button appear to reset scores when one score reaches 5
+function resetGame(){
+    const results= document.querySelector("#results")
+
+    const resetButton= document.createElement("button")
+    resetButton.textContent = "Reset Game";
+    resetButton.id = "resetButton"
+    results.appendChild(resetButton)
+
+    resetButton.addEventListener("click",()=>{
+        humanScore= 0
+        computerScore = 0
+        scoreCard(humanScore, computerScore)
+
+        resetButton.remove();
+    })
+}
+
+getHumanChoice()
+
